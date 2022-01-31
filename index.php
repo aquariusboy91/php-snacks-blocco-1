19afa6095974f4cbb2e18107d988d6d32cf61ed1
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-31 17:08:28 +0100

    snack 5

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ altrimenti “Accesso negato” -->
      DD-MM-YYYY es 01-01-2007 e come valore un array 
      di post associati a quella data. Stampare ogni data con i relativi post. -->
 
+     <!-- ## Snack 5
+Prendere un paragrafo abbastanza lungo, 
+contenente diverse frasi. Prendere il paragrafo e
+ suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
 <?php
     // $name = $_GET['name'];
     // $mail = $_GET['mail'];
@@ -61,15 +66,24 @@ altrimenti “Accesso negato” -->
     // print_r($posts);
 
 
-    $mynum = [];
-    $i = 0;
-    while ($i <= 15) { 
-        $rand = rand(1,100);
-        $mynum[] = $rand;
-        echo $mynum[$i] . ' ' ;
-        $i++; 
+    // $mynum = [];
+    // $i = 0;
+    // while ($i <= 15) { 
+    //     $rand = rand(1,100);
+        
+    //     if (!in_array($rand, $mynum)) {
+    //         $mynum[] = $rand;
+    //         echo $mynum[$i] . '<br> ' ;
+    //         $i++;
+    //     } 
+    // }
+
+    $paragrafo = "Molteplici furono i passi che portarono alla nascita di questa disciplina. Il primo, sia a livello di importanza sia di ordine cronologico, è l’avvento dei calcolatori e il continuo interesse rivolto a essi. Già nel 1623, grazie a Willhelm Sickhart, si arrivò a creare macchine in grado di effettuare calcoli matematici con numeri fino a sei cifre, anche se non in maniera autonoma. Nel 1642 Blaise Pascal costruì una macchina in grado di fare operazioni utilizzando il riporto automatico, mentre nel 1674 Gottfried Wilhelm von Leibniz creò una macchina in grado di effettuare la somma, la differenza e la moltiplicazione in maniera ricorsiva. Tra il 1834 e il 1837 Charles Babbage lavorò al modello di una macchina chiamata macchina analitica, le cui caratteristiche anticiparono in parte quelle dei moderni calcolatori. Nel ventesimo secolo l’attenzione sui computer ritornò ad accendersi: nel 1937, ad esempio, Claude Shannon, all’università di Yale, mostrò come l’algebra booleana e le operazioni binarie potessero rappresentare il cambiamento circuitale all’interno dei telefoni.";
+    $pieces = explode(".", $paragrafo);
+    for ($i = 0; $i < count($pieces); $i++) {
+        echo $pieces[$i] . '<br> ' ;
     }
-  
+
 ?>
 
 <!DOCTYPE html>
